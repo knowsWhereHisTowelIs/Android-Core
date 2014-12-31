56c5b02c92ebdb92ccfaa79d2d9e29be85e21563
knowsWhereHisTowelIs <cscalebslater@gmail.com>
2014-12-30 22:35:09 -0500

    Working splah

--- com/color/chooser/screens/MenuScreen.java
+++ com/color/chooser/screens/MenuScreen.java
@@ -1,5 +1,90 @@
 package com.color.chooser.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.color.chooser.game.ColorChooserGame;
+import com.color.chooser.gameworld.GameRenderer;
+import com.color.chooser.gameworld.GameWorld;
+import com.color.chooser.helpers.InputHandler;
+
 public class MenuScreen {
+
+	private ColorChooserGame game;
+	private SpriteBatch batcher;
+	private Sprite sprite;
+	private Texture splashTexture;
+	
+	private GameWorld world;
+	private GameRenderer renderer;
+	
+	private float runtime;
+	
+	public MenuScreen(ColorChooserGame colorChooserGame) {
+		this.game = colorChooserGame;
+		runtime = 0;
+		
+		float screenWidth = Gdx.graphics.getWidth();
+		float screenHeight = Gdx.graphics.getHeight();
+		float gameWidth = 136;
+		float gameHeight = screenHeight / (screenWidth / gameWidth);
+		int midPointY = (int) (gameHeight / 2);
+
+		world = new GameWorld(midPointY);
+		Gdx.input.setInputProcessor(new InputHandler(world, screenWidth / gameWidth, screenHeight / gameHeight));
+		renderer = new GameRenderer(world, (int) gameHeight, midPointY);
+		world.setRenderer(renderer);
+	}
+
+	@Override
+	public void show() {
+		batcher = new SpriteBatch();
+        splashTexture = new Texture(Gdx.files.internal("data/logo.png"));
+	}
+
+	@Override
+	public void render(float delta) {
+		runtime += delta;
+		
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batcher.begin();
+        batcher.draw(splashTexture, 0, 0);
+        batcher.end();
+        
+//        if(Gdx.input.justTouched())
+//                game.setScreen(new GameScreen());
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
